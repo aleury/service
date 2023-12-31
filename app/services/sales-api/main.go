b1127d8b927a9014b904644a83eebca9cdeee746
Adam Eury <euryadam@gmail.com>
2023-12-31 01:37:06 -0500

    Start api server and gracefully handle shutdown.

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
@@ -18,7 +19,7 @@ import (
 
 // This is the application version number.
 // It will be set at build time.
-var build string = "develop"
+var build = "develop"
 
 func main() {
 	log, err := logger.New("SALES-API")
@@ -38,6 +39,7 @@ func main() {
 func run(log *zap.SugaredLogger) error {
 	// ------------------------------------------------------------------
 	// Configuration
+
 	cfg := struct {
 		conf.Version
 		Web struct {
@@ -94,14 +96,48 @@ func run(log *zap.SugaredLogger) error {
 	}()
 
 	// ------------------------------------------------------------------
+	// Start API Service
+
+	log.Infow("startup", "status", "initializing v1 api support")
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 
-	sig := <-shutdown
+	api := http.Server{
+		Addr:         cfg.Web.APIHost,
+		Handler:      nil,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
+		IdleTimeout:  cfg.Web.IdleTimeout,
+		ErrorLog:     zap.NewStdLog(log.Desugar()),
+	}
 
-	log.Infow("shutdown", "status", "shutdown started", "signal", sig)
-	defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Infow("startup", "status", "api router started", "host", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// ------------------------------------------------------------------
+	// Shutdown
+
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.Web.ShutdownTimeout)
+		defer cancel()
+
+		if err := api.Shutdown(ctx); err != nil {
+			api.Close()
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
+	}
 
 	return nil
 }
