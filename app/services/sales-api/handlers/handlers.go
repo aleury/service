8e645899b8a4c256b3a1346a40e8cf6f5686fe35
Adam Eury <euryadam@gmail.com>
2024-01-09 02:28:10 -0500

    Add database setup in sales-api app startup.

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aleury/service/business/web/auth"
 	"github.com/aleury/service/business/web/v1/mid"
 	"github.com/aleury/service/foundation/web"
+	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
 )
 
@@ -16,6 +17,7 @@ type APIMuxConfig struct {
 	Shutdown chan os.Signal
 	Log      *zap.SugaredLogger
 	Auth     *auth.Auth
+	DB       *sqlx.DB
 }
 
 // APIMux construct a http.Handler with all application routes defined.
